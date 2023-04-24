8f533649622a384f497ad1d60a2c125e06e4fc9f
Miracle Ukpongson <miracleukpongs77@gmail.com>
2023-04-24 20:41:21 +0200

    new lines for local running added to app.py

--- app.py
+++ app.py
@@ -32,6 +32,6 @@ def create_app(db_url=None):
     return app
 
 
-# if __name__ == "__main__":
-#     app = create_app()
-#     app.run()
+if __name__ == "__main__":
+    app = create_app()
+    app.run()
